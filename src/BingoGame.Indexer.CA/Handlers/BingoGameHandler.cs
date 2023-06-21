3dac565228df6a9ccdacb020dc628c0f2b6236dc
xibo <xibo@portkey.finance>
2023-06-21 10:07:09 +0800

    fix: add handler

---
+++ src/BingoGame.Indexer.CA/Handlers/BingoGameHandler.cs
@@ -0,0 +1,25 @@
+using AElfIndexer.Client.Handlers;
+using AElfIndexer.Client.Providers;
+using AElfIndexer.Grains.State.Client;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Volo.Abp.ObjectMapping;
+
+namespace Portkey.Indexer.CA.Handlers;
+
+public class BingoGameHandler : TransactionDataHandler
+{
+    public BingoGameHandler(IClusterClient clusterClient, IObjectMapper objectMapper,
+        IAElfIndexerClientInfoProvider aelfIndexerClientInfoProvider, IDAppDataProvider dAppDataProvider,
+        IBlockStateSetProvider<TransactionInfo> blockStateSetProvider,
+        IDAppDataIndexManagerProvider dAppDataIndexManagerProvider,IEnumerable<IAElfLogEventProcessor<TransactionInfo>> processors,
+        ILogger<BingoGameHandler> logger) : base(clusterClient, objectMapper, aelfIndexerClientInfoProvider,
+        dAppDataProvider,blockStateSetProvider,dAppDataIndexManagerProvider,processors, logger)
+    {
+    }
+
+    protected override Task ProcessTransactionsAsync(List<TransactionInfo> transactions)
+    {
+        return Task.CompletedTask;
+    }
+}
